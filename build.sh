74a0fca3729385c94a7ec9af1fa7f0785ab76452
Michael Perry <lmperry@stanford.edu>
2017-06-07 16:54:27 -0700

    remove build helper script

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Builds the container.
-# The container can be exported using the export.sh script
-CONTAINER=flywheel/mriqc
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-docker build --tag $CONTAINER $DIR
